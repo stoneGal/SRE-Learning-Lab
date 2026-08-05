74eb93bfa2b0994c9ec0df3c54bcccb032f3ad14
bethel <bashorunbethel99@gmail.com>
2026-08-05 13:19:17 +0200

    Add ACM certificate, HTTPS listener, HTTP to HTTPS redirect

--- terraform/main.tf
+++ terraform/main.tf
@@ -326,6 +326,24 @@ resource "aws_lb_listener" "web" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "web_https" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate_validation.web.certificate_arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web.arn
   }
